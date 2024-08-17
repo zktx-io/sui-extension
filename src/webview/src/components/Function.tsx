d729d50d5ae7084a74953f1f49f8a077216ac0ed
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-08-18 01:25:16 +0900

    Update Function.tsx

--- src/webview/src/components/Function.tsx
+++ src/webview/src/components/Function.tsx
@@ -27,10 +27,14 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     padding: '8px 12px',
+    justifyContent: 'space-between',
   },
   title: {
     marginTop: '0px',
     marginBottom: '0px',
+  },
+  arrow: {
+    transition: 'transform 0.3s ease',
   },
   contentWrapper: {
     overflow: 'hidden',
@@ -105,6 +109,24 @@ export const Function = ({
       <div style={{ flexDirection: 'column' }}>
         <div style={styles.titleBar} onClick={toggleCard}>
           <div style={styles.title}>{name}</div>
+          <div
+            style={{
+              ...styles.arrow,
+              transform: isOpen ? 'rotate(90deg)' : 'rotate(0deg)',
+            }}
+            dangerouslySetInnerHTML={{
+              __html: `
+              <?xml version="1.0" encoding="iso-8859-1"?>
+                <svg fill="currentColor" height="8px" width="8px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                viewBox="0 0 330 330" xml:space="preserve">
+                <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+                c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+                C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+                C255,161.018,253.42,157.202,250.606,154.389z"/>
+              </svg>
+              `,
+            }}
+          />
         </div>
         <div
           style={{
