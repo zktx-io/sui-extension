50038109a586dde513527f714b1192a9a192be32
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-08-22 21:03:27 +0900

    Create faucet.ts

---
+++ src/webview/src/utilities/faucet.ts
@@ -0,0 +1,17 @@
+import { getFaucetHost, requestSuiFromFaucetV0 } from '@mysten/sui/faucet';
+import { IAccount } from '../recoil';
+
+export const faucet = async (account: IAccount): Promise<boolean> => {
+  if (account.nonce.network !== 'mainnet' && account.zkAddress) {
+    try {
+      const res = await requestSuiFromFaucetV0({
+        host: getFaucetHost(account.nonce.network),
+        recipient: account.zkAddress?.address,
+      });
+      return !res.error;
+    } catch (error) {
+      return false;
+    }
+  }
+  return false;
+};
