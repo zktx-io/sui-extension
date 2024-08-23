1d73857c32718546eee47e382e9e370cfec33c4e
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-08-23 11:05:54 +0900

    Update createProof.ts

--- src/webview/src/utilities/createProof.ts
+++ src/webview/src/utilities/createProof.ts
@@ -1,7 +1,5 @@
 import { getExtendedEphemeralPublicKey, jwtToAddress } from '@mysten/zklogin';
 import { Ed25519PublicKey } from '@mysten/sui/keypairs/ed25519';
-import Hex from 'crypto-js/enc-hex';
-import SHA256 from 'crypto-js/sha256';
 import { INonce, NETWORK } from '../recoil';
 import { getEnokiSalt } from './enoki/getSault';
 import { Enoki } from './commends';
@@ -15,16 +13,13 @@ const getProverUrl = (network: NETWORK): string => {
   }
 };
 
-const getPath = (network: NETWORK, index?: number): string => {
-  return `zkpath:${network}:0`;
-};
-
 export const createProof = async (
   { network, expiration, randomness, publicKey }: INonce,
   jwt: string,
 ): Promise<{ address: string; proof: string; salt: string }> => {
+  const enoki = await getEnokiSalt(jwt);
+
   if (network === 'testnet') {
-    const enoki = await getEnokiSalt(jwt);
     const res = await fetch(`${Enoki.url}/zklogin/zkp`, {
       method: 'POST',
       headers: {
@@ -55,10 +50,7 @@ export const createProof = async (
     };
   }
 
-  const path = getPath(network);
-  const salt = `0x${SHA256(path).toString(Hex).slice(0, 32)}`;
-  const address = jwtToAddress(jwt, BigInt(salt));
-
+  const address = jwtToAddress(jwt, BigInt(enoki.salt));
   const res = await fetch(getProverUrl(network), {
     method: 'POST',
     headers: {
@@ -71,14 +63,14 @@ export const createProof = async (
       ),
       maxEpoch: expiration,
       jwtRandomness: randomness,
-      salt,
+      salt: enoki.salt,
       keyClaimName: 'sub',
     }),
   });
 
   if (res.ok) {
     const data = await res.json();
-    return { address, proof: JSON.stringify(data), salt };
+    return { address, proof: JSON.stringify(data), salt: enoki.salt };
   } else {
     throw new Error('Login Fail - create proof error');
   }
